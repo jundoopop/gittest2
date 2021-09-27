30406cbc6aa057cb834ce5b00be9b661f5b6db3b
jundoopop <jundolpop@gmail.com>
2021-09-27 21:31:44 +0900

    first commit

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,30 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+    public String getMemberRepository;
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
+
+
+
